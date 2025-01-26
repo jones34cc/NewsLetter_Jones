ad8b68f7dccc0a22796bbaf2c7c5ecb488afa256
jones34cc <godzoroastrian@gmail.com>
2025-01-26 13:19:04 +0530

    added the signup for jones34cc

---
+++ app.js
@@ -0,0 +1,21 @@
+const express=require("express");
+const bodyparser=require("body-parser");
+const request=require("request");
+const app=express();
+
+app.use(express.static("public"));
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+"/signup.html");
+})
+
+
+
+
+
+
+
+app.listen(3000,function()
+{
+    console.log("Server is listening on port 3000.");
+})
