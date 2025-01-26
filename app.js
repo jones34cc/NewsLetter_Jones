0f914769f7ea4402e81ec9a47197170d1873a40c
jones34cc <godzoroastrian@gmail.com>
2025-01-26 14:49:24 +0530

    �successfully booted the server

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ const request=require("request");
 const app=express();
 
 app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
+
 app.get("/",function(req,res)
 {
     res.sendFile(__dirname+"/signup.html");
-})
+});
+app.post("/",function(req,res)
+{
+    var firstname=req.body.fname;
+    var secondname=req.body.lname;
+    var email=req.body.email;
+
+
+
+    console.log(firstname,secondname,email);
+
+
+
+});
+
 
 
 
